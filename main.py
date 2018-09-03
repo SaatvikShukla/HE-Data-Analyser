48c2dab113d180f9be5d8e739411c31d1f24de74
Saatvik Shukla <me@saatvikshukla.com>
2018-09-03 16:58:52 +0530

    Update: optimize code

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ Data filtering script to target cities/country/etc
 # North America
 targetCountries = ["Antigua and Barbuda", "Bahamas", "Barbados", "Belize", "Canada", "Costa Rica", "Cuba", "Dominica", "Dominican Republic", "El Salvador", "Grenada", "Guatemala", "Haiti", "Honduras", "Jamaica", "Mexico", "Nicaragua", "Panama", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Trinidad and Tobago", "USA"]
 
-TARGET_FILE = "bigdata.xls"
+TARGET_FILE = "ZS-BigData-Challenge-Summer-2018-result.xls"
 NoneType = type(None)
 
 import pandas as pd
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="HackerearthDataAnalyser", timeout=5)
-
+loc = 2
 def printIntro():
     print("===============")
     print(" Data Analyser")
@@ -25,14 +25,16 @@ def printIntro():
     print("Targetting: North America\n\n")
 
 def loader():
-    targetExcel = pd.read_excel(TARGET_FILE, header=None, skiprows = 1)
-    targetExcel.columns = ['Name','Email','Graduation_Year','Contact','Resume','Profile_link','Experience','Colleges','Company','Designation','Location','Gender','Timestamp','Referral Code', 'Are you interested in working for ZS?']
+    targetExcel = pd.read_excel(TARGET_FILE, header=None)
+    header = targetExcel.iloc[0]
+    targetExcel = targetExcel[1:]
+    targetExcel.rename(columns = header)
     return targetExcel
 
 
 def classifier(targetExcel, location, index, row):
     if(location[-1].strip() in targetCountries):
-            print(index, "yes ",location)
+        print(index, "yes ",location)
     else:
         targetExcel.drop(index, inplace=True)
         print(index, "no  ",location)
@@ -50,15 +52,16 @@ def categorizer(targetExcel, location, index, row, varStatus):
 
 def bootstrap(targetExcel):
     for index, row in targetExcel.iterrows():
-        location = geolocator.geocode(row['Location'])
+        location = geolocator.geocode(row[loc])
         if (type(location) == NoneType):
-            location = list(str(row['Location']).split(","))
+            location = list(str(row[loc]).split(","))
             categorizer(targetExcel, location, index, row, 1)
         else:
             categorizer(targetExcel, location, index, row, 0)
     return targetExcel
 
 def save(targetExcel):
+    print(len(targetExcel.index)," people in total from TARGET LOCATION")
     targetExcel = (targetExcel.reset_index())
     writer = pd.ExcelWriter('output.xlsx')
     targetExcel.to_excel(writer,'Sheet1')
