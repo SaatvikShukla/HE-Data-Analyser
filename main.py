11c217c132d5c632998cb032f5aebdf85972dd1a
Saatvik Shukla <me@saatvikshukla.com>
2018-09-03 15:47:18 +0530

    Refactor Code, fix Anamolies

--- main.py
+++ main.py
@@ -11,32 +11,52 @@ from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="HackerearthDataAnalyser", timeout=5)
 
+# North America
 targetCountries = ["Antigua and Barbuda", "Bahamas", "Barbados", "Belize", "Canada", "Costa Rica", "Cuba", "Dominica", "Dominican Republic", "El Salvador", "Grenada", "Guatemala", "Haiti", "Honduras", "Jamaica", "Mexico", "Nicaragua", "Panama", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Trinidad and Tobago", "USA"]
+targetFile = "bigdata.xls"
 
-targetExcel = pd.read_excel("bigdata.xls", header=None, skiprows = 1)
+def printIntro():
+    print("===============")
+    print(" Data Analyser")
+    print("===============")
+    print("TargetFile: ", targetFile)
+    print("Targetting: North America\n\n")
+
+def categorizer(location, index, row, varStatus):
+    """ Takes geolocator location object, TargetRow
+        Classifies and drop the rows not required based ono targetCountries
+    """
+    if(varStatus == 0):
+        location = location.address.split(",")
+        if(location[-1].strip() in targetCountries):
+            print(index, "yes ",location)
+        else:
+            targetExcel.drop(index, inplace=True)
+            print(index, "no  ",location)
+    else:
+        print("Could not deciper location, Brutality Mode Enabled.")
+        if(location[-1].strip() in targetCountries):
+            print(index, "yes ",location)
+        else:
+            targetExcel.drop(index, inplace=True)
+            print(index, "no  ",location)
+
+printIntro()
+targetExcel = pd.read_excel(targetFile, header=None, skiprows = 1)
 targetExcel.columns = ['Name','Email','Graduation_Year','Contact','Resume','Profile_link','Experience','Colleges','Company','Designation','Location','Gender','Timestamp','Referral Code', 'Are you interested in working for ZS?']
 NoneType = type(None)
-count = 1
+
 for index, row in targetExcel.iterrows():
     location = geolocator.geocode(row['Location'])
     if (type(location) == NoneType):
-        print("========")
-        print(count,location," is NoneType")
-        print("========")
-        targetExcel.drop(index, inplace=True)
-        count = count +  1
-        pass
+        location = list(str(row['Location']).split(","))
+        categorizer(location, index, row, 1)
+        #targetExcel.drop(index, inplace=True)
     else:
-        location = location.address.split(",")
-        if(location[-1].strip() in targetCountries):
-            print(count, "yes ",location)
-            count = count +  1
-        else:
-            targetExcel.drop(index, inplace=True)
-            print(count, "no  ",location)
-            count = count +  1
+        categorizer(location, index, row, 0)
+        
             
-targetExcel = (targetExcel)
+targetExcel = (targetExcel.reset_index())
 writer = pd.ExcelWriter('output.xlsx')
 targetExcel.to_excel(writer,'Sheet1')
 writer.save()
