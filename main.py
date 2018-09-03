4205d65e92b2f5730c980f78e226ba40e4961e60
Saatvik Shukla <me@saatvikshukla.com>
2018-09-03 16:22:50 +0530

    Main: Refactor, optimize

--- main.py
+++ main.py
@@ -6,57 +6,68 @@ Saatvik Shukla
 Data filtering script to target cities/country/etc
 """
 
+# North America
+targetCountries = ["Antigua and Barbuda", "Bahamas", "Barbados", "Belize", "Canada", "Costa Rica", "Cuba", "Dominica", "Dominican Republic", "El Salvador", "Grenada", "Guatemala", "Haiti", "Honduras", "Jamaica", "Mexico", "Nicaragua", "Panama", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Trinidad and Tobago", "USA"]
+
+TARGET_FILE = "bigdata.xls"
+NoneType = type(None)
+
 import pandas as pd
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="HackerearthDataAnalyser", timeout=5)
 
-# North America
-targetCountries = ["Antigua and Barbuda", "Bahamas", "Barbados", "Belize", "Canada", "Costa Rica", "Cuba", "Dominica", "Dominican Republic", "El Salvador", "Grenada", "Guatemala", "Haiti", "Honduras", "Jamaica", "Mexico", "Nicaragua", "Panama", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Trinidad and Tobago", "USA"]
-targetFile = "bigdata.xls"
-
 def printIntro():
     print("===============")
     print(" Data Analyser")
     print("===============")
-    print("TargetFile: ", targetFile)
+    print("TARGET_FILE: ", TARGET_FILE)
     print("Targetting: North America\n\n")
 
-def categorizer(location, index, row, varStatus):
+def loader():
+    targetExcel = pd.read_excel(TARGET_FILE, header=None, skiprows = 1)
+    targetExcel.columns = ['Name','Email','Graduation_Year','Contact','Resume','Profile_link','Experience','Colleges','Company','Designation','Location','Gender','Timestamp','Referral Code', 'Are you interested in working for ZS?']
+    return targetExcel
+
+
+def classifier(targetExcel, location, index, row):
+    if(location[-1].strip() in targetCountries):
+            print(index, "yes ",location)
+    else:
+        targetExcel.drop(index, inplace=True)
+        print(index, "no  ",location)
+
+def categorizer(targetExcel, location, index, row, varStatus):
     """ Takes geolocator location object, TargetRow
         Classifies and drop the rows not required based ono targetCountries
     """
     if(varStatus == 0):
         location = location.address.split(",")
-        if(location[-1].strip() in targetCountries):
-            print(index, "yes ",location)
-        else:
-            targetExcel.drop(index, inplace=True)
-            print(index, "no  ",location)
+        classifier(targetExcel, location, index, row)
     else:
         print("Could not deciper location, Brutality Mode Enabled.")
-        if(location[-1].strip() in targetCountries):
-            print(index, "yes ",location)
+        classifier(targetExcel, location, index, row)
+
+def bootstrap(targetExcel):
+    for index, row in targetExcel.iterrows():
+        location = geolocator.geocode(row['Location'])
+        if (type(location) == NoneType):
+            location = list(str(row['Location']).split(","))
+            categorizer(targetExcel, location, index, row, 1)
         else:
-            targetExcel.drop(index, inplace=True)
-            print(index, "no  ",location)
+            categorizer(targetExcel, location, index, row, 0)
+    return targetExcel
 
-printIntro()
-targetExcel = pd.read_excel(targetFile, header=None, skiprows = 1)
-targetExcel.columns = ['Name','Email','Graduation_Year','Contact','Resume','Profile_link','Experience','Colleges','Company','Designation','Location','Gender','Timestamp','Referral Code', 'Are you interested in working for ZS?']
-NoneType = type(None)
+def save(targetExcel):
+    targetExcel = (targetExcel.reset_index())
+    writer = pd.ExcelWriter('output.xlsx')
+    targetExcel.to_excel(writer,'Sheet1')
+    writer.save()
 
-for index, row in targetExcel.iterrows():
-    location = geolocator.geocode(row['Location'])
-    if (type(location) == NoneType):
-        location = list(str(row['Location']).split(","))
-        categorizer(location, index, row, 1)
-        #targetExcel.drop(index, inplace=True)
-    else:
-        categorizer(location, index, row, 0)
-        
-            
-targetExcel = (targetExcel.reset_index())
-writer = pd.ExcelWriter('output.xlsx')
-targetExcel.to_excel(writer,'Sheet1')
-writer.save()
+def init():
+    printIntro()
+    targetExcel = loader()
+    targetExcel = bootstrap(targetExcel)
+    save(targetExcel)
+    
+init() 
